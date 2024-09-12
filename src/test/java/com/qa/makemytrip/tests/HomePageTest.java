3cedab519b6a6b1d5497942cfcce2abfaaa7ee8b
Abhijit Khuntia <abhijitplaystation@gmail.com>
2024-09-12 08:07:33 +0530

    new run

--- src/test/java/com/qa/makemytrip/tests/HomePageTest.java
+++ src/test/java/com/qa/makemytrip/tests/HomePageTest.java
@@ -1,6 +1,8 @@
 package com.qa.makemytrip.tests;
 
 
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -8,6 +10,7 @@ import org.testng.annotations.Test;
 import com.qa.makemytrip.base.BaseTest;
 import com.qa.makemytrip.constants.Constants;
 
+import io.qameta.allure.Attachment;
 import io.qameta.allure.Description;
 import io.qameta.allure.Epic;
 import io.qameta.allure.Severity;
@@ -42,8 +45,8 @@ public class HomePageTest extends BaseTest {
 	@Test(dataProvider="countryData")
 	public void selectCountry(String country)
 	{
-		String CountryName="Bahrain";
-		Assert.assertEquals(true,homePage.selectCountryFromAllCountries(CountryName));
+		Assert.assertEquals(true,homePage.selectCountryFromAllCountries(country));
+		screenshot();
 	}
 	
 	@Test
@@ -94,5 +97,9 @@ public class HomePageTest extends BaseTest {
 		Assert.assertEquals(homePage.imageVisibility(),true);
 	}
 	
+	@Attachment(value = "Screenshot", type = "image/png")
+	public byte[] screenshot() {
+	    return ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+	}
 	
 }
